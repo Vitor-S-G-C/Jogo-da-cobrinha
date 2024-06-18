3ba0bfa95904e6a2353421c9ba9498778be4ef78
vitor gamarano <146540200+Vitor-S-G-C@users.noreply.github.com>
2024-06-18 00:49:14 -0300

    Esta quase
    Falta corrigir uns bugs

--- script.js
+++ script.js
@@ -1,36 +1,130 @@
 window.onload = function () {
-    canvas = document.getElementById("canva");
-    ctx = canvas.getcontext("2d");
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
 
 
-    //vou cria uma variavel 
-    var cobra = [];
-
-    var positionx = 10;
-
-    var positiony = 10;
-
-    var comidax = 15;
-
-    var comiday = 15;
-
-    var velx = 0;
-
-    var vely = 0;
-
-    var grade = 20;
-
-    var tam = 3;
+    //vou cria uma variavel cobra = [];
+    cobra = []
+    positionx = 10;
+    positiony = 10;
+    comidax = 15;
+    comiday = 15;
+    velx = 0;
+    vely = 0;
+    grade = 20;
+    tam = 3;
 
 
     //Esse comando vai chamar a função jogo a cada 100 Milesegundos
     setInterval(jogo, 100)
 
+
+    //Controles
+
+    document.addEventListener("keydown", function (e) {
+        switch (e.keyCode) {
+            //O numero 39 se referece ao lado direito
+            case 39:
+                velx = 1;
+                vely = 0;
+                break;
+            //O numero 37 se referece ao lado esquerda
+            case 37:
+                velx = -1;
+                vely = 0;
+                break;
+            //O numero 38 se referece para cima
+            case 38:
+                vely = -1;
+                velx = 0;
+                break;
+            //O numero 40 se referece para baixo
+            case 40:
+                vely = 1;
+                velx = 0;
+                break;
+
+
+        }
+    })
+
 }
 function jogo() {
     // config de tela do jogo
-    ctx.fillStyle = "#15b307";
-    ctx.filleReact(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = "#2980B9";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    //dESLOCAMENTO DA COBRA
+    positionx += velx;
+    positiony += vely;
+
+    //POSSIÇÃO DA COBRA
+    cobra.push({ x: positionx, y: positiony }),
+        console.log(cobra[0])
+
+    //Espelhamento 
+    if (positionx < 0) {
+        positionx = 29
+    }
+    else if (
+        positionx > 29) {
+        positionx = 0
+    }
+    else if (positiony > 29) {
+        positiony = 0
+        positionx++
+    }
+    else if (positiony < 0) {
+        positiony = 29
+    }
+
+
+
+    //Configurar a cobra
+    ctx.fillStyle = "#02bf03";
+
+
+    for (let i = 0; i < cobra.length; i++) {
+        //movimentação da cobra
+
+
+        //comportamento da cobra
+        ctx.fillRect(cobra[i].x * grade, cobra[i].y * grade, grade - 5, grade - 5);
+        if (cobra(i).x == positionx && cobra(i).y == positiony) {
+            tam = 3
+        }
+    }
+
+
+
+    //Limitando a cobra
+
+    while (cobra.length > tam) {
+        cobra.shift();
+
+    }
+
+    // comida
+    ctx.fillStyle = "#f1c40f"
+    ctx.fillRect(comidax * grade, comiday * grade, grade - 8, grade - 8
+    )
+
+    //comendo 
+    if (positionx == comidax && positiony == comiday) {
+        tam++;
+        comidax = Math.floor(Math.random() * grade);
+        comiday = Math.floor(Math.random() * grade);
+
+
+
+    }
 
 
 }
+
+
+
+
+
+
+
